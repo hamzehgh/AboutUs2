28ebe4681fe7ccbef4c0f24b7968a738ed7529f2
HamzehGhanem <hamzehghanemhh@gmail.com>
2020-10-23 14:39:34 +0300

    3omorko la fhemto

---
+++ app/src/main/java/com/example/aboutus2/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.aboutus2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+   public void BackButton(View view)
+    {
+        Intent i = new Intent(this,AskForRecord.class);
+        startActivity(i);
+    }
+
+
+}
