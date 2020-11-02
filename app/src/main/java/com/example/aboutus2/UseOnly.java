18e1493cc72ab0426c40b8a11fa81917b0ad818e
Hamzeh Ghanem <hamzehghanemhh@gmail.com>
2020-11-02 23:09:26 +0200

    3omorko la fhemto

---
+++ app/src/main/java/com/example/aboutus2/UseOnly.java
@@ -0,0 +1,14 @@
+package com.example.aboutus2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UseOnly extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_use_only);
+    }
+}
